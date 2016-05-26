651f96c69c86a702c2876193190f6474f8a1be2f
Ed A <eaponte@zoho.com>
2016-05-26 13:44:25 -0400

    Changed book titles in header

--- header.php
+++ header.php
@@ -38,7 +38,7 @@
 
 				<h1 class="hidden-xs"><a href="index.html" title="">Rosanna Chiofalo</a></h1>
 
-				<p>AUTHOR of <em>BELLA FORTUNA, CARISSIMA</em> and <em>STELLA MIA</em></p>
+				<p>AUTHOR of <em>ROSALIA'S BITTERSWEET PASTRY SHOP</em></p>
 
 			</div>
 
